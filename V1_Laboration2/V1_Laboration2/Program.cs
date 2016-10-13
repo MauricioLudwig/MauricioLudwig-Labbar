e6346875c2fd10c117969f9b3c945b742841c564
MauricioLudwig <mrdamiengallagher@gmail.com>
2016-10-13 04:03:24 +0200

    Added content

--- V1_Laboration2/V1_Laboration2/Program.cs
+++ V1_Laboration2/V1_Laboration2/Program.cs
@@ -10,6 +10,13 @@ namespace V1_Laboration2
     {
         static void Main(string[] args)
         {
+
+            // Creating an instance of Runtime and calling the start method
+            var runtime = new Runtime();
+            runtime.Start();
+
+            Console.WriteLine("You have reached the end of the program");
+            Console.ReadKey();
         }
     }
 }
---
+++ V1_Laboration2/V1_Laboration2/Runtime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V1_Laboration2
+{
+    class Runtime
+    {
+
+        public void Start()
+        {
+
+        }
+    }
+}
