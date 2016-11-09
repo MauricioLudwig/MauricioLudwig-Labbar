7c668f15d791c793f04f57d12130bdaed98ffcf0
MauricioLudwig <mrdamiengallagher@gmail.com>
2016-11-09 09:39:56 +0100

    initial upload

--- V5_Laboration9/V5_Laboration9/Program.cs
+++ V5_Laboration9/V5_Laboration9/Program.cs
@@ -8,10 +8,17 @@ namespace V5_Laboration9
 {
 
     // Vecka 5, Laboration 9
+    // Tic Tac Toe
     class Program
     {
         static void Main(string[] args)
         {
+            Game game = new Game);
+            game.Start();
+
+            Console.Clear();
+            Console.WriteLine("You have reached the end of the program.");
+            Console.ReadKey();
         }
     }
 }
