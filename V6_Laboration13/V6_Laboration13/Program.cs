28e735d7919d1da4d743b13d303d8d1e1952711d
MauricioLudwig <mrdamiengallagher@gmail.com>
2016-11-14 18:48:13 +0100

    Initial commit

---
+++ V6_Laboration13/V6_Laboration13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V6_Laboration13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
