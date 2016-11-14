f51d538e8a6ec978517c1c0b1f88b47ff4d176a2
MauricioLudwig <mrdamiengallagher@gmail.com>
2016-11-14 11:42:02 +0100

    Assignment complete

---
+++ V6_Laboration11/V6_Laboration11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V6_Laboration11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runtime runtime = new Runtime();
+            runtime.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ V6_Laboration11/V6_Laboration11/Runtime.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V6_Laboration11
+{
+
+    class Runtime
+    {
+
+        public delegate void AnalyzeNumber(int number);
+        public event AnalyzeNumber NumberInput;
+
+        public void Start()
+        {
+            Console.Write("Input a number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            NumberInput += new AnalyzeNumber(IsEven);
+            NumberInput += new AnalyzeNumber(IsDivisibleByThree);
+            NumberInput += new AnalyzeNumber(IsPrimeNumber);
+
+            OnNumberInput(input);
+        }
+
+        private void OnNumberInput(int input)
+        {
+            NumberInput?.Invoke(input);
+        }
+
+        public void IsEven(int number)
+        {
+            Console.WriteLine("{0} is {1} even number.", number, number % 2 == 0 ? "an" : "not an");
+        }
+
+        public void IsDivisibleByThree(int number)
+        {
+            Console.WriteLine("{0} {1} divisible by three.", number, number % 3 == 0 ? "is" : "is not");
+        }
+
+        public void IsPrimeNumber(int number)
+        {
+            bool isPrime = true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine("{0} {1} a prime number.", number, isPrime == true ? "is" : "is not");
+        }
+
+    }
+}
